ff6baff6b676291077604b13dc8a54a1ef710507
José Vasconcelos <zevasconcelos6@gmail.com>
2023-09-01 07:48:55 +0100

    updates

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.area-1',{
+    rotate: {x: 0, y: 100, z: 0},
+    duration: 1500
+});
+
+sr.reveal('.myResume',{duration: 1500});
+
+sr.reveal('#skills',{duration: 1500});
+
+sr.reveal('#contact',{duration: 1500});
