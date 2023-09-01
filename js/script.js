569b59f91bb3d49dbac0567ed1d753eebf805afc
José Vasconcelos <zevasconcelos6@gmail.com>
2023-09-01 10:28:50 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ sr.reveal('.myResume',{duration: 1500});
 sr.reveal('#skills',{duration: 1500});
 
 sr.reveal('#contact',{duration: 1500});
+
+function typeWriter(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra,i) => {
+        setTimeout(function(){
+            elemento.innerHTML += letra;
+        }, 55*i)
+    });
+}
+
+
+const resume = document.querySelector('.resume');
+typeWriter(resume);
